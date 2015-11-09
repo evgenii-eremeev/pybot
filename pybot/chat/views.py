5b2fd25e83718542a7e09a2033c9e183a1a9946a
JayCrypto <jaycrypto@gmail.com>
2015-11-09 17:28:38 +0500

    add basic template

--- pybot/chat/views.py
+++ pybot/chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class Index(generic.View):
+    def get(self, request):
+        return render(request, 'chat/index.html')
--- pybot/pybot/urls.py
+++ pybot/pybot/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^chat/', include('chat.urls', namespace="chat")),
     url(r'^admin/', include(admin.site.urls)),
 ]
